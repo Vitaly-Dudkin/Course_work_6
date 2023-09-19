7cf00a2ce7faefb87f68b42ee9ce24a2ee6df010
vitaly <dudkin231@mail.ru>
2023-09-19 14:59:29 +0300

    added models.py

--- mailing/models.py
+++ mailing/models.py
@@ -1,36 +1,86 @@
 from django.db import models
 
 
+class Client(models.Model):
+
+    email = models.EmailField(unique=True, max_length=60, verbose_name='Email')
+    full_name = models.CharField(max_length=150, verbose_name='Full_name')
+    comment = models.TextField(null=True, blank=True, verbose_name='Comments')
+
+    phone = models.CharField(max_length=35, verbose_name='Phone number', null=True, blank=True)
+    avatar = models.ImageField(upload_to='users/', verbose_name='image', null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.full_name} {self.email}"
+
+    class Meta:
+        verbose_name = 'Client'
+        verbose_name_plural = 'Clients'
+        ordering = ('name',)
+
+
+class Message(models.Model):
+
+    subject = models.CharField(default='No subject', max_length=100, verbose_name='Title')
+    body = models.TextField(null=True, blank=True, verbose_name='Message')
+
+    def __str__(self):
+        return self.subject
+
+    class Meta:
+        verbose_name = 'Message'
+        verbose_name_plural = 'Messages'
+
+
 # Create your models here.
 class MailingSettings(models.Model):
-    STATUS_OK = 'ok'
-    STATUS_FAILED = 'failed'
+
+    choice_period = [
+        (1, 'once in a day'),
+        (7, 'once in a week'),
+        (31, 'once in a month'),
+    ]
+
     STATUSES = (
-        (STATUS_OK, 'Success'),
-        (STATUS_FAILED, 'Error'),
+        ('created', 'created'),
+        ('started', 'started'),
+        ('finished', 'finished')
     )
 
-    time_of_sending = models.DateTimeField(auto_now_add=True, verbose_name='time_of_sending')
-    client = models.ForeignKey('Client', on_delete=models.CASCADE, verbose_name='Client')
-    log_of_sending = models.CharField(choices=STATUSES, default=STATUS_OK, verbose_name='Status')
-    periodicity = models.ForeignKey('Status', on_delete=models.CASCADE, verbose_name='periodicity')
-    status = models.CharField(verbose_name='status')
-    message = models.ForeignKey('MessageMailing', max_length=300, on_delete=models.CASCADE, verbose_name='message')
+    start_time = models.DateTimeField(verbose_name='Start time')
+    end_time = models.DateTimeField(null=True, blank=True, verbose_name='End time')
+    interval = models.PositiveSmallIntegerField(choices=choice_period, verbose_name='interval')
+    status = models.CharField(choices=STATUSES, default='created', verbose_name='status')
+
+    clients = models.ManyToManyField(Client, verbose_name='Clients')  # ManyToManyField?
+    message = models.ForeignKey(Message, null=True, blank=True, on_delete=models.CASCADE, verbose_name='message')
+    # log =
+
+    def __str__(self):
+        return f'{self.message}: {self.status} ({self.interval})'
+
+    class Meta:
+        verbose_name = 'Newsletter'
+        verbose_name_plural = 'Newsletters'
 
 
-# class MailingLog:
-#     STATUS_OK = 'ok'
-#     STATUS_FAILED = 'failed'
-#     STATUSES = (
-#         (STATUS_OK, 'Success'),
-#         (STATUS_FAILED, 'Error'),
-#     )
-#
-#     last_try = models.DateTimeField(auto_now_add=True, verbose_name='Date of last try')
-#     # client = models.ForeignKey('Client', on_delete=models.CASCADE, verbose_name='Client')
-#     settings = models.ForeignKey(MailingSettings, on_delete=models.CASCADE, verbose_name='Settings')
-#     status = models.CharField(choices=STATUSES, default=STATUS_OK, verbose_name='Status')
-#
-#     class Meta:
-#         verbose_name = 'Log'
-#         verbose_name_plural = 'Logs'
+class MailingLog:
+
+    STATUSES = (
+        ('successful', 'Success'),
+        ('error', 'Error')
+    )
+
+    last_try = models.DateTimeField(auto_now_add=True, verbose_name='Last_try')
+    status = models.CharField(max_length=10, choices=STATUSES, verbose_name='Status_of_sending')
+    server_response = models.CharField(null=True, blank=True, verbose_name='Server_response')
+
+    mailing = models.ForeignKey(MailingSettings, on_delete=models.CASCADE, verbose_name='Newsletter')
+    client = models.ForeignKey(Client, on_delete=models.CASCADE, verbose_name='Client')
+
+    def __str__(self):
+        return f'{self.last_try}: {self.server_response}, {self.status}'
+
+    class Meta:
+        verbose_name = 'Log'
+        verbose_name_plural = 'Logs'
